6d438d2d7d0ba0a6648311fac855e81f4ce5fc97
Pierpaolo Gorelli <pierpaologorelli@MacBook-Pro-di-Pierpaolo.local>
2015-03-08 17:54:28 +0100

    Updated default theme

--- web/app/themes/elektro/template-services.php
+++ web/app/themes/elektro/template-services.php
@@ -3,58 +3,55 @@
 Template Name: Template Services
 */
 ?>
+test
 <div id="two-container">
-		<div id="brandandproduct-wrapper">
-			<div class="group" id="brand-group">
-				<div class="bandp-item bandp-circle" id="brand-shape">
-					<div class="text" id="brand-text">Brand</div>
-				</div>
+	<div id="brandandproduct-wrapper">
+		<div class="group" id="brand-group">
+			<div class="bandp-item bandp-circle" id="brand-shape">
+				<div class="text" id="brand-text">Brand</div>
 			</div>
+		</div>
 			
-			<div class="group" id="conj-group">
-				<div class="bandp-item" id="conj-shape">
-					<div class="text" id="conj-text">and</div>
-				</div>
+		<div class="group" id="conj-group">
+			<div class="bandp-item" id="conj-shape">
+				<div class="text" id="conj-text">and</div>
 			</div>
+		</div>
 			
-			<div class="group" id="product-group">
-				<div class="bandp-item bandp-circle" id="product-shape">
-					<div class="text" id="brand-text">Product</div>
-				</div>
+		<div class="group" id="product-group">
+			<div class="bandp-item bandp-circle" id="product-shape">
+				<div class="text" id="brand-text">Product</div>
 			</div>
 		</div>
 	</div>
-
-	<div id="slide-container" class="fixed">
-		<div class="snappish-main">
-			<?php
+</div>
+<div id="slide-container" class="fixed">
+	<div class="snappish-main">
+		<?php
 				
 				
-			?>
-			<article class="slide-1">
-				<section class="content-slide">
-					<div class="label">
-						<div class="wrap">
-							<small>This is</small>
-							<h2><strong>Code and Theory</strong></h2>
-						</div>
+		?>
+		<article class="slide-1">
+			<section class="content-slide">
+				<div class="label">
+					<div class="wrap">
+						<small>This is</small>
+						<h2><strong>Code and Theory</strong></h2>
 					</div>
-				</section>
-			</article>
+				</div>
+			</section>
+		</article>
 
-			<article class="slide-2">
-				<section class="content-slide">
-					<div class="label">
-						<div class="wrap">
-							<h2><strong>Ideas and Technology</strong></h2>
-						</div>
+		<article class="slide-2">
+			<section class="content-slide">
+				<div class="label">
+					<div class="wrap">
+						<h2><strong>Ideas and Technology</strong></h2>
 					</div>
-				</section>
-			</article>
-		</div>
+				</div>
+			</section>
+		</article>
 	</div>
+</div>
 
-	<a href="#" class="scroll-down-arrow"><span>TAP OR SCROLL</span><i class="arrow"></i></a>
-
-
-			
+<a href="#" class="scroll-down-arrow"><span>TAP OR SCROLL</span><i class="arrow"></i></a>
