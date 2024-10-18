c73cbffa47157efd581d76ec8e7bc0583886b009
lupin72 <pierpaolo.gorelli@gmail.com>
2024-10-18 18:28:02 +0200

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['"Inter variable"', 'sans-serif'],
+			display: ['"Work Sans"', 'sans-serif'],
+		},
+		extend: {},
+	},
+	plugins: [
+		require("@xpd/tailwind-3dtransforms")
+	],
+}
