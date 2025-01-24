8fbf39c7a9c2fd75a1a8bbcd4c241d4d30199eb5
lupin72 <pierpaolo.gorelli@gmail.com>
2025-01-24 03:29:56 +0100

    Fixed trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://piergorelli.com',
-  integrations: [tailwind(), sitemap(), partytown({ config: { forward: ["dataLayer.push"] } })]
+  integrations: [tailwind(), sitemap(), partytown({ config: { forward: ["dataLayer.push"] } })],
 });
