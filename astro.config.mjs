ba69d9af21f1f298ea7e5a9bbf3a6a65d2cd4efd
lupin72 <pierpaolo.gorelli@gmail.com>
2025-04-22 01:36:02 +0200

    Added reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown'
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://piergorelli.com',
   integrations: [tailwind(), sitemap(), partytown({ config: { forward: ["dataLayer.push"] } })],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
